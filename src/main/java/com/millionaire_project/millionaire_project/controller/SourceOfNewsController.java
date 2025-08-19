2048ed45b0d1adcebb2bbd38767fbb7f7ad97a1c
Tanghai Ly <tanghai.ly@wingbank.com.kh>
2025-08-19 11:53:57 +0700

    add api to download sample

--- src/main/java/com/millionaire_project/millionaire_project/controller/SourceOfNewsController.java
+++ src/main/java/com/millionaire_project/millionaire_project/controller/SourceOfNewsController.java
@@ -3,9 +3,15 @@ package com.millionaire_project.millionaire_project.controller;
 import com.millionaire_project.millionaire_project.service.SourceOfNewsService;
 import com.millionaire_project.millionaire_project.util.ResponseBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -24,6 +30,16 @@ public class SourceOfNewsController {
     public ResponseBuilder<List> response() {
       return service.response();
    }
+
+    @GetMapping("/download-sample")
+    public ResponseEntity<InputStreamResource> downloadSample() throws IOException {
+        ClassPathResource file = new ClassPathResource("src/main/resources/static/upcoming.xlsx");
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=upcoming.xlsx")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(new InputStreamResource(file.getInputStream()));
+    }
 
 
 }
